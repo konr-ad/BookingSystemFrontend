efa8843aa0ac6f0b638b6d00cf21e8638d933203
Konrad <konradk95@gmail.com>
2024-02-19 09:01:13 +0100

    adding layout to apartment component

--- src/app/models/apartment.dto.ts
+++ src/app/models/apartment.dto.ts
@@ -1,10 +1,12 @@
 // models/apartment.dto.ts
 
 export interface ApartmentDto {
-  id?: number;
+  id: number;
   number: string;
   pricePerNight: number;
   area: string;
   capacity: number;
+  reservationId?: number;
+  isEditing?: boolean;
 }
 
--- src/app/services/apartment/apartment.service.ts
+++ src/app/services/apartment/apartment.service.ts
@@ -7,7 +7,7 @@ import { ApartmentDto } from '../../models/apartment.dto';
   providedIn: 'root'
 })
 export class ApartmentService {
-  private apiUrl = 'http://localhost:8080/api/apartments'; // URL do Twojego API
+  private apiUrl = 'http://localhost:8080/api/apartments';
 
   constructor(private http: HttpClient) {}
 
@@ -17,5 +17,17 @@ export class ApartmentService {
 
   getAllApartments(): Observable<ApartmentDto[]> {
     return this.http.get<ApartmentDto[]>(this.apiUrl);
+  }
+
+  searchApartments(searchTerm: string): Observable<ApartmentDto[]> {
+    return this.http.get<ApartmentDto[]>(`${this.apiUrl}?search=${searchTerm}`);
+  }
+
+  updateApartment(apartment: ApartmentDto): Observable<ApartmentDto> {
+    return this.http.put<ApartmentDto>(`${this.apiUrl}/${apartment.id}`, apartment);
+  }
+
+  deleteApartment(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
