c6fb6cae9db75174d69d461cdc4f19c10b8922b8
Konrad <konradk95@gmail.com>
2024-02-19 09:01:00 +0100

    adding layout to apartment component

--- src/app/components/apartment/create/createApartment.component.ts
+++ src/app/components/apartment/create/createApartment.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApartmentService } from '../../../services/apartment/apartment.service';
 
 @Component({
   selector: 'app-create-apartment',
   templateUrl: './createApartment.component.html',
   styleUrls: ['./createApartment.component.css']
 })
-export class CreateApartmentComponent  {
+export class CreateApartmentComponent implements OnInit {
+  apartmentForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private apartmentService: ApartmentService) {}
+
+  ngOnInit(): void {
+    this.apartmentForm = this.fb.group({
+      number: ['', [Validators.required]],
+      pricePerNight: ['', [Validators.required, Validators.min(1)]],
+      area: ['', [Validators.required]],
+      capacity: ['', [Validators.required, Validators.min(1)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.apartmentForm.valid) {
+      // Wywołaj serwis do zapisania apartamentu
+      this.apartmentService.createApartment(this.apartmentForm.value).subscribe({
+        next: (apartment) => console.log(apartment),
+        error: (error) => console.error('Błąd podczas dodawania apartamentu', error)
+      });
+    } else {
+      Object.values(this.apartmentForm.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
+  }
 }
--- src/app/components/apartment/view/viewApartment.component.ts
+++ src/app/components/apartment/view/viewApartment.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+// components/apartment/view/viewApartment.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { ApartmentDto } from '../../../models/apartment.dto';
+import { ApartmentService } from '../../../services/apartment/apartment.service';
 
 @Component({
   selector: 'app-view-apartment',
   templateUrl: './viewApartment.component.html',
   styleUrls: ['./viewApartment.component.css']
 })
-export class ViewApartmentComponent {
+export class ViewApartmentComponent implements OnInit {
+  apartments: ApartmentDto[] = [];
+  searchTerm: string = '';
 
+  constructor(private apartmentService: ApartmentService) {}
+
+  ngOnInit(): void {
+    this.getAllApartments();
+  }
+
+  getAllApartments(): void {
+    this.apartmentService.getAllApartments().subscribe(apartments => {
+      this.apartments = apartments.map(apartment => ({
+        ...apartment,
+        isEditing: false // Dodajemy flagę do kontroli stanu edycji
+      }));
+    });
+  }
+
+  searchApartments(): void {
+    this.apartmentService.searchApartments(this.searchTerm).subscribe(apartments => {
+      this.apartments = apartments;
+    });
+  }
+
+  enableEdit(id: number): void {
+    this.apartments = this.apartments.map(apartment => {
+      if (apartment.id === id) apartment.isEditing = true;
+      return apartment;
+    });
+  }
+
+  saveApartment(apartment: ApartmentDto): void {
+    this.apartmentService.updateApartment(apartment).subscribe(() => {
+      this.getAllApartments(); // Odśwież listę po zapisie
+    });
+  }
+
+  deleteApartment(id: number): void {
+    this.apartmentService.deleteApartment(id).subscribe(() => {
+      this.getAllApartments(); // Odśwież listę po usunięciu
+    });
+  }
+
+  cancelEdit(): void {
+    this.apartments.forEach(apartment => {
+      apartment.isEditing = false;
+    });
+  }
 }
