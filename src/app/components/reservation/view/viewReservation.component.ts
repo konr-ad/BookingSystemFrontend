cef98006b25cfaec601510ece8893a458265434d
Konrad <konradk95@gmail.com>
2024-03-22 13:46:30 +0100

    implementing view reservations function

--- src/app/components/reservation/view/viewReservation.component.ts
+++ src/app/components/reservation/view/viewReservation.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../../../services/reservation/reservation.service';
+import { ReservationDto } from '../../../models/reservation.dto';
 
 @Component({
   selector: 'app-view-reservation',
   templateUrl: './viewReservation.component.html',
   styleUrls: ['./viewReservation.component.css']
 })
-export class ViewReservationComponent {
+export class ViewReservationComponent implements OnInit {
+  reservations: ReservationDto[] = [];
+  searchTerm: string = '';
 
+  constructor(private reservationService: ReservationService) {}
+
+  ngOnInit(): void {
+    this.getAllReservations();
+  }
+
+  getAllReservations(): void {
+    this.reservationService.getAllReservations().subscribe(reservations => {
+      this.reservations = reservations;
+    });
+  }
+
+  searchReservations(): void {
+    this.reservationService.searchReservations(this.searchTerm).subscribe(reservations => {
+      this.reservations = reservations;
+    });
+  }
 }
--- src/app/services/reservation/reservation.service.ts
+++ src/app/services/reservation/reservation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ReservationDto } from '../../models/reservation.dto';
 
@@ -14,5 +14,34 @@ export class ReservationService {
 
   createReservation(reservationData: ReservationDto): Observable<ReservationDto> {
     return this.http.post<ReservationDto>(this.apiUrl, reservationData);
+  }
+
+  getAllReservations(): Observable<ReservationDto[]> {
+    return this.http.get<ReservationDto[]>(this.apiUrl);
+  }
+
+  searchReservations(searchTerm: string, lastName?: string, phoneNumber?: string, status?: string): Observable<ReservationDto[]> {
+    let params = new HttpParams();
+    if (searchTerm) {
+      params = params.append('search', searchTerm);
+    }
+    if (lastName) {
+      params = params.append('lastName', lastName);
+    }
+    if (phoneNumber) {
+      params = params.append('phoneNumber', phoneNumber);
+    }
+    if (status) {
+      params = params.append('status', status);
+    }
+    return this.http.get<ReservationDto[]>(`${this.apiUrl}/search`, { params: params });
+  }
+
+  updateReservation(reservation: ReservationDto): Observable<ReservationDto> {
+    return this.http.put<ReservationDto>(`${this.apiUrl}/${reservation.id}`, reservation);
+  }
+
+  deleteReservation(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
